ddbd11d26976a5500e8809149e25d44c8b6c4eea
ryanchall99 <rileychall@hotmail.co.uk>
2023-02-14 17:21:46 +0000

    Added Nebula Hazard (Sprite), Stopped Scarfy Animation mid air.

--- main.cpp
+++ main.cpp
@@ -8,21 +8,22 @@ int main() {
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
     SetTargetFPS(60);
 
-    // TEXTURE
+    // SCARFY VARIABLES
     Texture2D scarfy = LoadTexture("textures/scarfy.png"); // Loading the sprite sheet
-    
-    Rectangle scarfyRec; // Creating a rectangle
-    scarfyRec.width = scarfy.width / 6; // Width: Spritesheet / 6 (6 Sprites)
-    scarfyRec.height = scarfy.height; // Height: Spritesheet Height
-    scarfyRec.x = 0; // Left
-    scarfyRec.y = 0; // Top
+    Rectangle scarfyRec = {0, 0, (float)scarfy.width / 6, (float)scarfy.height}; // Creating a rectangle
+    Vector2 scarfyPos = {windowWidth/2 - scarfyRec.width/2 , windowHeight - scarfyRec.height}; // Position of the sprite (Character)
+
+    // NEBULA VARIABLES
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebRec = {0.0, 0.0, (float)nebula.width/8, (float)nebula.height / 8};
+    Vector2 nebPos = {windowWidth, windowHeight - nebRec.height}; 
+
+    int nebVel = -600; // Nebula X Velocity (Pixels / s)
 
     // Animation frame
     int frame = 0;
     const float updateTime = 1.0 / 12.0; // Amount of time before we update the animation frame
     float runningTime = 0;
-    
-    Vector2 scarfyPos = {windowWidth/2 - scarfyRec.width/2 , windowHeight - scarfyRec.height}; // Position of the sprite (Character)
 
     float velocity = 0; // (Pixels / Frame)
     const int jumpVel = -600; // (Pixels / Sec)
@@ -56,27 +57,39 @@ int main() {
             // Check for jumping
             if(IsKeyPressed(KEY_SPACE) && !isInAir) { velocity += jumpVel; } // If the space key is pressed & not in the air, we apply jump velocity to the overall velocity
             
-            // Update Y Position
+            // Update Scarfy Y Position
             scarfyPos.y += velocity * dT; // Update the rectangles Y position with the velocity value.
 
-            // Update Animation Frame
-            runningTime += dT; // Update runningTime
-            if(runningTime >= updateTime) {
-                runningTime = 0;
+            // Update Nebula X Position
+            nebPos.x += nebVel * dT;
+            // if(nebPos.x < 0) {
+            //     nebPos.x = windowWidth;
+            // }
 
-                scarfyRec.x = frame * scarfyRec.width;
-                frame++;
-                if(frame > 5) {
-                    frame = 0;
+            // Update Animation Frame
+            if(!isInAir) 
+            {
+                runningTime += dT; // Update runningTime
+                if(runningTime >= updateTime) {
+                    runningTime = 0;
+
+                    scarfyRec.x = frame * scarfyRec.width;
+                    frame++;
+                    if(frame > 5) {
+                        frame = 0;
+                    }
                 }
             }
 
-            DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+            // Texture Drawing
+            DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE); // Scarfy
+            DrawTextureRec(nebula, nebRec, nebPos, WHITE); // Nebula
 
         EndDrawing(); // End of Drawing
     }
     
     UnloadTexture(scarfy); // Unloading Texture
+    UnloadTexture(nebula); // Unloading Texture
     CloseWindow(); // Closes Window and unloads OpenGL context
 
     return 0;
